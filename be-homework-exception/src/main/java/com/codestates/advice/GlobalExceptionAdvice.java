e8667c3b770e3dd9fb8b35f100abc6dc62a290dd
kwang <hea0408never@naver.com>
2022-08-25 16:08:59 +0900

    result

---
+++ be-homework-exception/src/main/java/com/codestates/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,56 @@
+package com.codestates.advice;
+
+import com.codestates.exception.BusinessLogicException;
+import com.codestates.exception.ExceptionCode;
+import com.codestates.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(
+            ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
+        System.out.println(e.getExceptionCode().getStatus());
+        System.out.println(e.getMessage());
+
+        // TODO GlobalExceptionAdvice 기능 추가 1
+        return new ResponseEntity<>(ErrorResponse.of(e.getExceptionCode()),HttpStatus.valueOf(e.getExceptionCode().getStatus()));
+    }
+
+    // TODO GlobalExceptionAdvice 기능 추가 2
+    @ExceptionHandler
+    public ResponseEntity handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
+        return new ResponseEntity(ErrorResponse.of(ExceptionCode.METHOD_NOT_ALLOWED),HttpStatus.valueOf(ExceptionCode.METHOD_NOT_ALLOWED.getStatus()));
+    }
+
+    // TODO GlobalExceptionAdvice 기능 추가 3
+    @ExceptionHandler
+    public ResponseEntity handleException(NullPointerException e){
+        return new ResponseEntity(ErrorResponse.of(ExceptionCode.INTERNAL_SERVER_ERROR),HttpStatus.valueOf(ExceptionCode.INTERNAL_SERVER_ERROR.getStatus()));
+    }
+}
