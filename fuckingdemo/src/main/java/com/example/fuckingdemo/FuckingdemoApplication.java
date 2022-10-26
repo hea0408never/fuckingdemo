ba68bf5ebd5c56e93040ae9b4c57075b9b7f755d
kwang <hea0408never@naver.com>
2022-10-26 11:47:05 +0900

    1

---
+++ fuckingdemo/src/main/java/com/example/fuckingdemo/FuckingdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuckingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuckingdemoApplication.class, args);
+	}
+
+}
---
+++ fuckingdemo/src/test/java/com/example/fuckingdemo/FuckingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuckingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuckingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
